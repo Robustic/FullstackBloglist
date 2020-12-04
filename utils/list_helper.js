c38ca9eb557f3a8a2624e27be15e297921d71b6c
Juha Malinen <juha.malinen@helsinki.fi>
2020-12-04 12:24:18 +0200

    Bloglist 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,17 @@
 const dummy = (blogs) => {
-
+    console.log(blogs)
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const totalLikesForAllBlogs = (sum, blog) => {
+        return sum + blog.likes
+    }
+
+    return blogs.reduce(totalLikesForAllBlogs, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
