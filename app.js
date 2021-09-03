4c19c27dc143ed04aa10d526745e5a9b50af9fed
Juha Malinen <juha.malinen@helsinki.fi>
2021-09-03 13:24:47 +0300

    Bloglist 4.15

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 const mongoUrl = config.MONGODB_URI
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(mongoUrl)
     .then(() => {
         logger.info('connected to MongoDB')
     })
@@ -24,6 +25,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
